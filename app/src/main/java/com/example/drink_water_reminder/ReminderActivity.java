533a9c2ea557a0d659d3b0293601889315e0ddd4
minhduc6 <72613060+minhduc6@users.noreply.github.com>
2022-03-18 00:33:25 +0700

    update

---
+++ app/src/main/java/com/example/drink_water_reminder/ReminderActivity.java
@@ -0,0 +1,13 @@
+package com.example.drink_water_reminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReminderActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminder);
+    }
+}
