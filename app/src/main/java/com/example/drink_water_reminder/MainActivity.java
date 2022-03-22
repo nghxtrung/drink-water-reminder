78e4ed24ce73b39cbcb7becb660c6b331cef29ba
Giangk50 <giangcoivipk50@gmail.com>
2022-03-22 22:23:16 +0700

    layout Welcome

--- app/src/main/java/com/example/drink_water_reminder/MainActivity.java
+++ app/src/main/java/com/example/drink_water_reminder/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.drink_water_reminder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, WelcomeActivity.class);
+        startActivity(intent);
+    }
+
+    private void StartACtivity(Intent intent) {
     }
 }
--- app/src/main/java/com/example/drink_water_reminder/WelcomeActivity.java
+++ app/src/main/java/com/example/drink_water_reminder/WelcomeActivity.java
@@ -2,7 +2,11 @@ package com.example.drink_water_reminder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.widget.TimePicker;
+
+import java.text.BreakIterator;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -11,4 +15,6 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
     }
+
+
 }
